ee400affa601a01d715e2667cea870c462aa2540
Out of Ink Software <moshekrum@gmail.com>
2015-12-13 23:44:06 -0500

    initial commit
    basic usage

---
+++ Niflaot MiTorahtecha/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace Niflaot_MiTorahtecha
+{
+    partial class mainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gregBox = new System.Windows.Forms.CheckBox();
+            this.lateBox = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.yearInput = new System.Windows.Forms.NumericUpDown();
+            this.goButton = new System.Windows.Forms.Button();
+            this.locationText = new System.Windows.Forms.Label();
+            this.pasukText = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.yearInput)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gregBox
+            // 
+            this.gregBox.AutoSize = true;
+            this.gregBox.Location = new System.Drawing.Point(120, 12);
+            this.gregBox.Name = "gregBox";
+            this.gregBox.Size = new System.Drawing.Size(97, 17);
+            this.gregBox.TabIndex = 0;
+            this.gregBox.Text = "Gregorian Year";
+            this.gregBox.UseVisualStyleBackColor = true;
+            this.gregBox.CheckedChanged += new System.EventHandler(this.gregBox_CheckedChanged);
+            // 
+            // lateBox
+            // 
+            this.lateBox.AutoSize = true;
+            this.lateBox.Enabled = false;
+            this.lateBox.Location = new System.Drawing.Point(120, 35);
+            this.lateBox.Name = "lateBox";
+            this.lateBox.Size = new System.Drawing.Size(130, 17);
+            this.lateBox.TabIndex = 1;
+            this.lateBox.Text = "After Rosh HaShanah";
+            this.lateBox.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Year:";
+            // 
+            // yearInput
+            // 
+            this.yearInput.Location = new System.Drawing.Point(50, 20);
+            this.yearInput.Maximum = new decimal(new int[] {
+            5888,
+            0,
+            0,
+            0});
+            this.yearInput.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.yearInput.Name = "yearInput";
+            this.yearInput.Size = new System.Drawing.Size(52, 20);
+            this.yearInput.TabIndex = 4;
+            this.yearInput.Value = new decimal(new int[] {
+            5776,
+            0,
+            0,
+            0});
+            // 
+            // goButton
+            // 
+            this.goButton.Location = new System.Drawing.Point(297, 58);
+            this.goButton.Name = "goButton";
+            this.goButton.Size = new System.Drawing.Size(75, 23);
+            this.goButton.TabIndex = 5;
+            this.goButton.Text = " Go";
+            this.goButton.UseVisualStyleBackColor = true;
+            this.goButton.Click += new System.EventHandler(this.goButton_Click);
+            // 
+            // locationText
+            // 
+            this.locationText.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.locationText.Location = new System.Drawing.Point(40, 84);
+            this.locationText.Margin = new System.Windows.Forms.Padding(0);
+            this.locationText.Name = "locationText";
+            this.locationText.Size = new System.Drawing.Size(300, 45);
+            this.locationText.TabIndex = 6;
+            this.locationText.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pasukText
+            // 
+            this.pasukText.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pasukText.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pasukText.Location = new System.Drawing.Point(40, 129);
+            this.pasukText.Margin = new System.Windows.Forms.Padding(0);
+            this.pasukText.Name = "pasukText";
+            this.pasukText.Size = new System.Drawing.Size(300, 100);
+            this.pasukText.TabIndex = 7;
+            this.pasukText.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // mainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 238);
+            this.Controls.Add(this.pasukText);
+            this.Controls.Add(this.locationText);
+            this.Controls.Add(this.goButton);
+            this.Controls.Add(this.yearInput);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lateBox);
+            this.Controls.Add(this.gregBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "mainWindow";
+            this.Text = "Niflaot MiTorahtecha";
+            ((System.ComponentModel.ISupportInitialize)(this.yearInput)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox gregBox;
+        private System.Windows.Forms.CheckBox lateBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown yearInput;
+        private System.Windows.Forms.Button goButton;
+        private System.Windows.Forms.Label locationText;
+        private System.Windows.Forms.Label pasukText;
+    }
+}
+
---
+++ Niflaot MiTorahtecha/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Niflaot_MiTorahtecha
+{
+
+    public partial class mainWindow : Form
+    {
+        public string pasuk;
+
+        public mainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void gregBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (gregBox.Checked)
+            {
+                lateBox.Enabled = true;
+                yearInput.Minimum = -3761;
+                yearInput.Value -= 3760;
+                if (lateBox.Checked) { yearInput.Value -= 1; }
+                yearInput.Maximum = 2128;
+            }
+            if (!gregBox.Checked) {
+                lateBox.Enabled = false;
+                yearInput.Maximum = 5888;
+                yearInput.Value += 3760;
+                if (lateBox.Checked) { yearInput.Value += 1; }
+                yearInput.Minimum = 0;
+            }
+
+        }
+
+        private void goButton_Click(object sender, EventArgs e)
+        {
+            int number = Convert.ToInt32(yearInput.Value);
+            string book;
+
+            if (gregBox.Checked) {
+                number += 3760;
+                if (lateBox.Checked) { number += 1; }
+            }
+            if (number > 4878)
+            {
+                number -= 4878;
+                book = "Devarim";
+                pasuk = File.ReadLines("devarim.txt").Skip(number - 1).Take(1).First();
+            }
+            else if (number > 3589)
+            {
+                number -= 3589;
+                book = "Bamidbar";
+                pasuk = File.ReadLines("bamidbar.txt").Skip(number - 1).Take(1).First();
+            }
+            else if (number > 2730)
+            {
+                number -= 2730;
+                book = "Vayikra";
+                pasuk = File.ReadLines("vayikra.txt").Skip(number - 1).Take(1).First();
+            }
+            else if (number > 1533)
+            {
+                number -= 1533;
+                book = "Shemot";
+                pasuk = File.ReadLines("shemot.txt").Skip(number - 1).Take(1).First();
+            }
+            else {
+                book = "Bereshis";
+                pasuk = File.ReadLines("bereshis.txt").Skip(number - 1).Take(1).First();
+
+            }
+            locationText.Text = book + pasuk.Substring(0, 10);
+            pasukText.Text = pasuk.Substring(10);
+        }
+
+    }
+}
